b9947e7ff5164524d3c92fc69f117878901e6d3b
srijan041 <gunzerker41@gmail.com>
2024-07-11 18:23:22 +0530

    removed logs

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -6,6 +6,4 @@ const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
 
-console.log("BASE URL: ", axiosInstance.defaults.baseURL);
-
 export default axiosInstance;
